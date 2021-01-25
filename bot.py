0c66c995f08520dd9d2e078be16fa9a5577063fc
Brian <bacornelius5@gmail.com>
2021-01-24 21:07:31 -0800

    messing around with discord.py, transferring to desktop

--- bot.py
+++ bot.py
@@ -3,6 +3,11 @@ import os
 
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
+import pymongo
+from pymongo import MongoClient
+
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -11,6 +16,26 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == guild:
+            break
+    
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    reply = 'Hello! Test Complete'
+
+    if message.content == 'test!':
+        await message.channel.send(reply)
 
 client.run(TOKEN)
