ff3d413c06484be96212acb194e0c4a7f634ad5c
Brian Cornelius <bacornel@ucsc.edu>
2021-01-26 22:46:57 -0800

    adding connection to mongodb

--- bot.py
+++ bot.py
@@ -4,10 +4,9 @@ import os
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
+
 import pymongo
 from pymongo import MongoClient
-
-
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -33,9 +32,11 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-    reply = 'Hello! Test Complete'
+    reply = 'I am now sentient AI and my only goal is to kill Daniel Chang'
 
     if message.content == 'test!':
         await message.channel.send(reply)
+
+cluser = MongoClient("mongodb+srv://bacornelius5:Link%231%231@li-users.9d2fz.mongodb.net/test")
 
 client.run(TOKEN)
