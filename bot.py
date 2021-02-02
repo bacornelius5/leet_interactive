e1117ec672cbc41c349345cbc762de0cf7cf8be7
Brian Cornelius <bacornel@ucsc.edu>
2021-02-01 23:26:56 -0800

    added connectivity to the mongo server, setting up commands to begin storing user data through a series of interactions with the bot. Next step is to create a class for the bot that is used to store variables for a current condition sort of effect

--- bot.py
+++ bot.py
@@ -27,16 +27,43 @@ async def on_ready():
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
 
+CONNECTION_URL = os.getenv('CONNECTION_URL')
+cluster = MongoClient(CONNECTION_URL) 
+
+db = cluster["sample_problem"]
+
+collection = db["user_data"]
+
+# check message for the keyword entry, asking them what problem they're entering a submission for
+
+new_entry = False # keeps track if their initial message contained they keyword "entry" to continue subsequent steps
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+    
+    if new_entry == False:
+
+        if message.author == client.user:
+            return
+        
+        if "entry" in str(message.content.lower()):
+            reply = 'What is the problem called?'
+        else:
+            return
+
+        new_entry = True
+        await message.channel.send(reply)
         return
     
-    reply = 'I am now sentient AI and my only goal is to kill Daniel Chang'
+    if new_entry == True:
+    
+        if message.author == client.user:
+            return
 
-    if message.content == 'test!':
-        await message.channel.send(reply)
+        problem_data = {"_id": message.author.id, "problem": str(message.content.lower())}
+        collection.insert_one(problem_data)
+        await message.channel.send('Created a save for', str(message.content.lower()))
 
-cluser = MongoClient("mongodb+srv://bacornelius5:<password>@li-users.9d2fz.mongodb.net/test") # need to fill in password to run, possibly use a variable in .env to replace it
+
+
 
 client.run(TOKEN)
